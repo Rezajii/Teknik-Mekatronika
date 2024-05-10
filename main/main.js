864eebbadaed8fae09d4fbe1efdcc2440a914375
Rezajii <ahmadrezaa2006@gmail.com>
2024-05-10 09:19:07 +0800

    Feat: Information

--- main/main.js
+++ main/main.js
@@ -1,8 +1,33 @@
 const menu = document.querySelector(".menu");
 const nav = document.querySelector(".items");
+let displays = document.querySelectorAll(".num");
+let interval = 1500;
+const info = document.querySelector("#info");
 
 menu.addEventListener("click", () => {
   nav.classList.toggle("h-0");
 });
 
 AOS.init();
+
+window.onscroll = () => {
+  let top = window.scrollY;
+  let offset = info.clientTop;
+  let height = info.clientHeight;
+
+  if (top >= offset && top < offset + height) {
+    displays.forEach((display) => {
+      let startvalue = 0;
+      let endvalue = parseInt(display.getAttribute("data-val"));
+      let duration = Math.floor(interval / endvalue);
+
+      let counter = setInterval(function () {
+        startvalue += 1;
+        display.textContent = startvalue;
+        if (startvalue == endvalue) {
+          clearInterval(counter);
+        }
+      }, duration);
+    });
+  }
+};
